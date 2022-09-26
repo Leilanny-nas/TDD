90cfb6d1810bb42c1cf8fc93fea36fa2c813b472
Leilanny <leilanny.nascimento.oliveira07@aluno.ifce.edu.br>
2022-09-26 17:14:43 -0300

    first commit

---
+++ src/main/java/Livraria/interfaces/Book.java
@@ -0,0 +1,7 @@
+package Livraria.interfaces;
+
+public interface Book {
+    public String setTitulo(String titulo);
+    public String setIsbn(String isbn);
+    public double setPreco(double preco);
+}
---
+++ src/main/java/Livraria/interfaces/ShoppingCart.java
@@ -0,0 +1,9 @@
+package Livraria.interfaces;
+
+import java.lang.reflect.Array;
+
+public interface ShoppingCart {
+    public void addBook(Book book);
+    public double price();
+
+}
